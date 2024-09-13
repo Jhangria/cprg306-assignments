e36b381022385acc88b3960da9b52b68f2188413
Jhangria <rajvirsingh.jhangria@edu.sait.ca>
2024-09-13 11:32:46 -0600

    Week-2 Page Edited
    Made changes to the week 2 page.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const StudentInfo = () => {
+    return (
+        <div>
+            <h1>Rajvir Singh Jhangria</h1>
+            <p><Link href= "https://github.com/Jhangria/cprg306-assignments">Jhangria</Link></p>
+        </div>
+    );
+}
+export default StudentInfo;
+        
